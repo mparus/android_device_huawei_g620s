9bf8d3797b946ed50c1507b235733ca5fc4b2aa5
desalesouche <desalesouche@gmail.com>
2015-11-29 13:09:50 +0100

    delete ril test
    Change-Id: Ie138004ec0cb9e152244b7db23d7a3cfd9d6b753

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,10 +109,6 @@ ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
 ADDITIONAL_DEFAULT_PROPERTIES += ro.allow.mock.location=1
 ADDITIONAL_DEFAULT_PROPERTIES += persist.sys.usb.config=mass_storage
 
-# Flags
-COMMON_GLOBAL_CFLAGS += -DUSE_RIL_VERSION_10
-COMMON_GLOBAL_CPPFLAGS += -DUSE_RIL_VERSION_10
-
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
 BOARD_KERNEL_SEPARATED_DT := true
