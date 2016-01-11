95bd333b61c558cd61ce6216c98d5d4ed1128ca9
mparus <mikael.parus@gmail.com>
2016-01-11 12:51:39 +0100

    remove jar library keyhandle
    Change-Id: I22c77cb1a59e614b60b2a51ad2e3e2a9a1c55534

--- g620s.mk
+++ g620s.mk
@@ -230,8 +230,8 @@ PRODUCT_PACKAGES += \
     calmodule.cfg
 
 # Live Wallpapers
-PRODUCT_PACKAGES += \
-    com.cyanogenmod.keyhandler
+#PRODUCT_PACKAGES += \
+#    com.cyanogenmod.keyhandler
 
 # Ramdisk
 PRODUCT_PACKAGES += \
@@ -298,7 +298,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     sys.io.scheduler=bfq
 
-PRODUCT_SYSTEM_SERVER_JARS += com.cyanogenmod.keyhandler
+#PRODUCT_SYSTEM_SERVER_JARS += com.cyanogenmod.keyhandler
 # never dexopt the keyhandler
 $(call add-product-dex-preopt-module-config,com.cyanogenmod.keyhandler,disable)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
